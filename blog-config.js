5a9df673bc1de05d6d32e7c6f89b136ca50f8047
mybabygrand <amuro_ray@kakao.com>
2022-08-22 23:23:02 +0900

    modify blog-config.js

--- blog-config.js
+++ blog-config.js
@@ -2,7 +2,7 @@ module.exports = {
   title: "majorTom",
   description: "Ground control to major Tom",
   author: "majorTom",
-  siteUrl: "https://myBabyGrand.github.io",
+  siteUrl: "https://mybabygrand.github.io/",
   links: {
     github: "https://github.com/mybabygrand",
     linkedIn: "https://linkedin.com",
@@ -10,7 +10,7 @@ module.exports = {
     resume: "http://www.naver.com"
   },
   utterances: {
-    repo: "myBabyGrand/myBabyGrand.github.io",
+    repo: "mybabygrand/mybabygrand.github.io",
     type: "pathname",
   },
 }

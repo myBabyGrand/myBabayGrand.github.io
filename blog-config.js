0f1b01b46cb9fa82b9795153111c07a845a478df
mybabygrand <amuro_ray@kakao.com>
2022-08-22 22:41:00 +0900

    init

---
+++ blog-config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  title: "majorTom",
+  description: "Ground control to major Tom",
+  author: "majorTom",
+  siteUrl: "https://mybabygrand.github.io/gatsby-starter-hoodie/",
+  links: {
+    github: "https://github.com/mybabygrand",
+    linkedIn: "https://linkedin.com",
+    email: "mailto:amuro_ray@kakao.com",
+    resume: "http://www.naver.com"
+  },
+  utterances: {
+    repo: "devHudi/gatsby-starter-hoodie",
+    type: "pathname",
+  },
+}
